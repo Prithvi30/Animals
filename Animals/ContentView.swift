e078d0297ed5884e96bf292a6203fe3882a05d79
Prithvi Raj <Prithvi2229@gmail.com>
2022-11-13 14:16:01 +0530

    Initial Commit

---
+++ Animals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animals
+//
+//  Created by Prithviraj Patil on 13/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
